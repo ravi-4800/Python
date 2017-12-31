f598ba3c132ec25c828c52d5a1fc3e5f1dab9b22
ravi-4800 <ravi.yadav14598@gmail.com>
2017-12-31 23:50:21 +0530

    python practice

--- last_occurence.py
+++ last_occurence.py
@@ -5,10 +5,11 @@ while t>0:
     q = int(input())
     for i in range(q):
         x = input()
-        #print(x)
+    
         if x not in arr_ele:
             print(-1)
         else:
-            print(max([arr_ele.index(i) for i in arr_ele if i == x])+1)      
+            #print(max([arr_ele.index(i) for i in arr_ele if i == x])+1)
+            print(max([i for i in range(n) if arr_ele[i] == x])+1)
     t=t-1
         
