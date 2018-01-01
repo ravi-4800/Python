d4ce1ed7f93737195904c907895136db3a6d44b4
ravi-4800 <ravi.yadav14598@gmail.com>
2018-01-01 11:49:34 +0530

    by using list comprehension

--- last_occurence.py
+++ last_occurence.py
@@ -5,9 +5,8 @@ while t>0:
     q = int(input())
     for i in range(q):
         x = input()
-    
         if x not in arr_ele:
             print(-1)
         else:
-            print(max([arr_ele.index(i) for i in arr_ele if i == x])+1)
+            print(max([i for i in range(n) if arr_ele[i] == x])+1)
     t=t-1
