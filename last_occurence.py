b739b1a941657ecb60741ed3f76645b295f23921
ravi-4800 <ravi.yadav14598@gmail.com>
2018-01-01 10:28:08 +0530

    std i/p & o/p

--- last_occurence.py
+++ last_occurence.py
@@ -1,15 +1,17 @@
-t = int(input())
+from sys import stdin,stdout
+t = int(stdin.readline())
 while t>0:
-    n = int(input())
-    arr_ele = input().split(' ')
-    q = int(input())
+    n = int(stdin.readline())
+    arr_ele = stdin.readline().split()
+    q = int(stdin.readline())
     for i in range(q):
-        x = input()
-    
-        if x not in arr_ele:
-            print(-1)
+        x = stdin.readline().split()
+        #print(x)
+        if x[0] not in arr_ele:
+            stdout.write('-1\n')
         else:
-            #print(max([arr_ele.index(i) for i in arr_ele if i == x])+1)
-            print(max([i for i in range(n) if arr_ele[i] == x])+1)
+            a=max([i for i in range(n-1,-1,-1) if arr_ele[i] == x[0]])+1
+            stdout.write(str(a)+'\n')
+
     t=t-1
         
