26949b99ee49a750cebf92d9e11918bf3766fc5d
ravi-4800 <ravi.yadav14598@gmail.com>
2018-01-03 18:53:16 +0530

    simple method

--- bob_and_string.py
+++ bob_and_string.py
@@ -18,30 +18,30 @@ while T>0:
             dic2[x] = [True]
         else:
             dic2[x].append(True)
-    
-    temp=z=0
-    
-    for i in t:
-        if i in dic1:
+
+    temp = 0
+
+    for x in t:
+        if x in dic1:
             try:
-                del dic1[i][0]
+                del dic1[x][0]
             except IndexError:
-                temp=temp+2
-                z=z+1
-        else:
-            temp=temp+2
-            z=z+1
-            
-    if len(s)!=len(t):
-        for i in s:
-            if i not in dic2:
                 temp=temp+1
-            else:
-                try:
-                    del dic2[i][0]
-                except IndexError:
-                    temp=temp+1
-        temp = temp-z
-    
-    stdout.write(str(temp)+'\n')
+        else:
+            temp=temp+1
+                
+
+    for x in s:
+        if x in dic2:
+            try:
+                del dic2[x][0]
+            except IndexError:
+                temp=temp+1
+        else:
+            temp=temp+1
+
+    print(temp)
     T=T-1
+                
+    
+
