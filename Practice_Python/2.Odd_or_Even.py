d8cb454b9c1ffc5271289e57b7805cda3bf56e0b
ravi-4800 <ravi.yadav14598@gmail.com>
2018-01-11 09:30:12 +0530

    modulo operator

---
+++ Practice_Python/2.Odd_or_Even.py
@@ -0,0 +1,22 @@
+n = int(input("Enter a number: "))
+if n%2==0:
+    print("{} is an even number".format(n))
+else:
+    print("{} is an odd number".format(n))
+
+####Extras
+##
+##n = int(input("Enter a number: "))
+##if n%4==0:
+##    print("{} is multiple of 4".format(n))
+##elif n%2==0:
+##    print("{} is an even number".format(n))
+##else:
+##    print("{} is an odd number".format(n))
+##
+##num = int(input("Enter a number to check: "))
+##check = int(input("Enter a divisor: "))
+##if num % check ==0:
+##    print("{} evenly divides {}".format(check,num))
+##else:
+##    print("{} is not divisible by {}".format(num,check))
