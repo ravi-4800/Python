6a10bf1d7d474412f83213a9eafc454502752af4
ravi-4800 <ravi.yadav14598@gmail.com>
2018-01-13 09:34:01 +0530

    reverse the string

---
+++ Practice_Python/6.String_Lists.py
@@ -0,0 +1,23 @@
+s = input("Enter a string: ")
+i=0
+j=-1
+temp = 0
+while i<=len(s)//2-1 and j>=-(len(s)//2):
+    if s[i]==s[j]:
+        temp = temp+1
+        print(temp)
+    else:
+        break
+    i = i+1
+    j = j-1
+if temp == len(s)//2:
+    print("String is palindrome")
+else:
+    print("String is not palindrome")
+
+## using reverse string
+p = input("Enter a string: ")
+if (p == p[::-1]):
+    print("String is palindrome")
+else:
+    print("String is not palindrome")
