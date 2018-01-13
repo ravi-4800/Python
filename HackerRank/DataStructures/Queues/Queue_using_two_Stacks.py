2f0efb62573a82fd2173b47f20da07f10a16a31a
ravi-4800 <ravi.yadav14598@gmail.com>
2018-01-13 17:45:54 +0530

    solved by using deque()

--- HackerRank/DataStructures/Queues/Queue_using_two_Stacks.py
+++ HackerRank/DataStructures/Queues/Queue_using_two_Stacks.py
@@ -1,16 +1,11 @@
-stack1 = []
-stack2 = []
+from collections import deque
 q = int(input())
+que = deque()
 while q>0:
     a = input().split(" ")
     if int(a[0])==1:
-        stack1.append(int(a[1]))
+        que.append(int(a[1]))
     elif int(a[0])==2:
-        stack2 = stack1[::-1]
-        stack2.pop()
-        stack1 = stack2[::-1]
+        que.popleft()
     else:
-        stack2 = stack1[::-1]
-        print(stack2[-1])
-        stack1 = stack2[::-1]
-    q=q-1
+        print(que[0])
